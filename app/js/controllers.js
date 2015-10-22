0b0a62f286af60c5c2d81df3769d13eb24eb9422
Ashley Street <ashley.street@digitaslbi.com>
2015-10-22 10:11:46 +0100

    Added some SASS styling and variables/Mixins, also fonts

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -7,10 +7,16 @@ var myApp = angular.module('myApp', []);
 myApp.controller('EmployeeListCtrl', function($scope) {
 	$scope.employees = [
 	{'name': 'Ashley Street',
-	 'role': 'Interface Developer'},
+	 'role': 'Interface Developer',
+	 'level': 'Graduate',
+	 'department':'Technology'},
 	{'name': 'Gary Wong',
-	 'role': 'Automation Tester'},
+	 'role': 'Automation Tester',
+	 'level': 'Midweight',
+	 'department': 'Technology'},
 	 {'name': 'Nader Aoudin',
-	  'role': 'Interface Developer'}
+	  'role': 'Interface Developer',
+	  'level': 'Senior',
+	  'department': 'Technology'}
 	];
 });
