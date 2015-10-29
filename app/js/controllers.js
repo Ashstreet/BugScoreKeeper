951e0180c92a301561de43a795aa3f72f33905bd
Ashley Street <ashley.street@digitaslbi.com>
2015-10-29 13:46:36 +0000

    Angular Ajax made and general feature play

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -4,19 +4,16 @@
 
 var myApp = angular.module('myApp', []);
 
-myApp.controller('EmployeeListCtrl', function($scope) {
-	$scope.employees = [
-	{'name': 'Ashley Street',
-	 'role': 'Interface Developer',
-	 'level': 'Graduate',
-	 'department':'Technology'},
-	{'name': 'Gary Wong',
-	 'role': 'Automation Tester',
-	 'level': 'Midweight',
-	 'department': 'Technology'},
-	 {'name': 'Nader Aoudin',
-	  'role': 'Interface Developer',
-	  'level': 'Senior',
-	  'department': 'Technology'}
-	];
+myApp.controller('EmployeeListCtrl', function($scope, $http) {
+	var url = '../json/EmployeeData.json';
+
+	$http({
+		method: 'GET',
+		url: url
+	}).success(function (data) {
+		$scope.employees = data;
+	}).error(function (data) {
+		console.log(data);
+	});
+	$scope.orderProp = 'id';
 });
